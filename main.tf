5d29fd22b8236abff02e9468a2922a795e895fb5
Sonali Arora <sonaliarora2012@icloud.com>
2023-01-31 10:42:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_redshift_cluster" "example" {
+  cluster_identifier = "tf-redshift-cluster"
+  database_name      = "mydb"
+  master_username    = "exampleuser"
+  master_password    = "Mustbe8characters"
+  node_type          = "dc1.large"
+  cluster_type       = "single-node"
+}
